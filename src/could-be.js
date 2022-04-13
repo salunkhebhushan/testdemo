34d33ed585f730e45e2579b44109f0bd6a3cd09b
salunkhebhushan <92618340+salunkhebhushan@users.noreply.github.com>
2022-04-13 13:36:02 +0530

    Update could-be.js

--- src/could-be.js
+++ src/could-be.js
@@ -3,12 +3,12 @@ let md;
 md = window.markdownit({html: true}).use(window.markdownitFootnote);
 // Load the Markdown file with jQuery.
 $.ajax({
-  url: "https://the-javascripting-english-major.org/v1/examples/markdown/hastings-street.md",
+  url: "https://github.com/salunkhebhushan/testdemo/README.md",
   success: function(markdown){
     // Convert the Markdown to HTML.
     let html;
     html = md.render(markdown);
     // Print the HTML to #content using jQuery.
-    $("#content").html(html);
+    $("#pageData").html(html);
   }
 });
